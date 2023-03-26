4481a58b6b5ee983a4c9e0f5482e7efcb0d998fc
joel oluwafisayo <oluwafisayojoel@gmail.com>
2023-03-26 18:49:32 +0100

    fix

--- giresii/src/Pages/Auth/Login.jsx
+++ giresii/src/Pages/Auth/Login.jsx
@@ -2,13 +2,12 @@ import "../../styles/Login/Login.scss";
 const Login = () => {
   return (
     <div className="login">
-       <div className="login-image">
-        <div className="login-image-cover"></div>
-      </div>
       <div className="login-details">
         <div className="login-fields"></div>
       </div>
-      
+      <div className="login-image">
+        <div className="login-image-cover"></div>
+      </div>
     </div>
   );
 };
