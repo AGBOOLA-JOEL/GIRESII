71016d8d9ecfee7fc4454b4beed054848ee7d814
joel oluwafisayo <oluwafisayojoel@gmail.com>
2023-04-02 18:38:04 +0100

    media query for login page done

--- giresii/src/Pages/Auth/Login.jsx
+++ giresii/src/Pages/Auth/Login.jsx
@@ -3,6 +3,7 @@ import { loginData } from "../../data/LoginCarousel";
 import "../../styles/Login/Login.scss";
 import { AiFillStar } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
+import BlackLogo from "Assets/Svg/BlackLogo";
 // import { Carousel } from "react-responsive-carousel";
 
 const Login = () => {
@@ -58,32 +59,37 @@ const Login = () => {
         </>
       </div>
       <div className="login-credentials">
-        <div className="login-credentials-header">
-          <p>Create an account</p>
-          <p>Let's get started with your blog credentials</p>
-        </div>
-
-        <div className="login-credentials-field">
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-        </div>
-
-        <div className="login-credentials-btn">
-          <div className="login-create">
-            <p>Create account</p>{" "}
+        <div className="login-credentials-container">
+          <div className="login-black-logo">
+            <BlackLogo />
           </div>
-          <div className="login-google">
-            <span>
-              <FcGoogle size={30} />
-            </span>
-            <p>Sign up with Google</p>
+          <div className="login-credentials-header">
+            <p>Create an account</p>
+            <p>Let's get started with your blog credentials</p>
           </div>
 
-          <div className="login-direct">
-            <p>
-              Already have an account?<span>Log in</span>
-            </p>
+          <div className="login-credentials-field">
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+          </div>
+
+          <div className="login-credentials-btn">
+            <div className="login-create">
+              <p>Create account</p>{" "}
+            </div>
+            <div className="login-google">
+              <span>
+                <FcGoogle size={30} />
+              </span>
+              <p>Sign up with Google</p>
+            </div>
+
+            <div className="login-direct">
+              <p>
+                Already have an account?<span>Log in</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
